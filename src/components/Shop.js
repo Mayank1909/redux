9c74b2d5f4db61cecf93eabc2cc9219088cd7088
Mayank1909 <78377998+Mayank1909@users.noreply.github.com>
2023-09-16 12:13:37 +0530

    understand redux

---
+++ src/components/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>Buy abibas shoes Rs. 1</h1><br />
+            <button className="btn btn-primary mx-3"> - </button>
+            Add this item tom cart
+            <button className="btn btn-primary mx-3"> + </button>
+
+        </div>
+    )
+}
+
+export default Shop
